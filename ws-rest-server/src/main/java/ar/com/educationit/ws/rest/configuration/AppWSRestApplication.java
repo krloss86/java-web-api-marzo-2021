e1bd10aa5f003d74e65ecd6a7e0cdeb5ef08e6d7
CHLopez <CHLopez@chlopez-ntb.boldt.com.ar>
2021-03-27 13:03:22 -0300

    clase3
    - ws rest funciona!

---
+++ ws-rest-server/src/main/java/ar/com/educationit/ws/rest/configuration/AppWSRestApplication.java
@@ -0,0 +1,7 @@
+package ar.com.educationit.ws.rest.configuration;
+
+import org.glassfish.jersey.server.ResourceConfig;
+
+public class AppWSRestApplication extends ResourceConfig {
+
+}
---
+++ ws-rest-server/src/main/java/ar/com/educationit/ws/rest/configuration/Configuration.java
@@ -0,0 +1,9 @@
+package ar.com.educationit.ws.rest.configuration;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class Configuration extends Application{
+
+}
