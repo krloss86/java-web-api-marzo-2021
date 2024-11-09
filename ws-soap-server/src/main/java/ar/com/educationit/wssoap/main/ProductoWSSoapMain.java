4b3c86fcf8e461d3035030ace9598eb7a13b689e
carlos horacio lopez <carloshoracio.lopez@comafi.com.ar>
2024-11-09 00:22:38 -0300

    add README.MD

--- ws-soap-server/src/main/java/ar/com/educationit/wssoap/main/ProductoWSSoapMain.java
+++ ws-soap-server/src/main/java/ar/com/educationit/wssoap/main/ProductoWSSoapMain.java
@@ -3,6 +3,9 @@ package ar.com.educationit.wssoap.main;
 import ar.com.educationit.wssoap.impl.ProductoWsSoapServiceImpl;
 import jakarta.xml.ws.Endpoint;
 
+/*
+ * Para ejecutar: java --add-opens java.base/java.lang=ALL-UNNAMED --add-opens java.base/java.lang.reflect=ALL-UNNAMED -jar target/ws-soap-server-1.0.0-SNAPSHOT.jar
+ */
 public class ProductoWSSoapMain {
 
 	public static void main(String[] args) {
